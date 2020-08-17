289f011ed8420d0cc0ceb076104b14330ee0c07b
cristiam <cristiamherrera@gmail.com>
2020-08-17 08:54:43 -0400

    Se adiciono el controlador hojaruta

---
+++ app/Http/Controllers/HojasRutaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HojasRutaController extends Controller
+{
+    public function registro()
+    {
+        echo "hojas desde hojas ruta";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// HOJAS DE RUTA
+Route::get('HojasRuta/registro', 'HojasRutaController@registro');
