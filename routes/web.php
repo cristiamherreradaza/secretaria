6b656bf42d477c970b4fb584a510c9d1f4af6398
cristiam <cristiamherrera@gmail.com>
2020-08-18 07:47:55 -0400

    Se adiciono el login

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 // HOJAS DE RUTA
 Route::get('HojasRuta/registro', 'HojasRutaController@registro');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
